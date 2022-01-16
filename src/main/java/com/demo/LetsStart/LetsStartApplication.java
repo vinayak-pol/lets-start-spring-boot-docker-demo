a8102e9174de1684afbeea8edf7f9264f4d903c4
vinayak-pol <vinayakpol111@gmail.com>
2022-01-17 00:23:45 +0530

    commit 2

--- src/main/java/com/demo/LetsStart/LetsStartApplication.java
+++ src/main/java/com/demo/LetsStart/LetsStartApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class LetsStartApplication {
-
+	//TEST
 	public static void main(String[] args) {
 		SpringApplication.run(LetsStartApplication.class, args);
 	}
